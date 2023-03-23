4b56d36a7c1e18b88fd40353b40eeb4dd5e6baff
EdRafCas <Eduardoo.rafael@gmail.com>
2023-03-22 23:57:46 -0300

    adding styled components and SASS compatibility

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './App.css';
 
 
 const App = () => {
   return ( 
-    <div>
-      Test 2
+    <div className='container' >
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+
     </div>
    );
 }
