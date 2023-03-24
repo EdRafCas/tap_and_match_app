81a4bf8b90fa5dc07ae1f51838f09d506554cf3b
EdRafCas <Eduardoo.rafael@gmail.com>
2023-03-23 23:59:11 -0300

    minor changes, to layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import './App.css';
+import './App.scss';
 
 
 const App = () => {
   return ( 
     <div className='container' >
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-
+      <div className='column number-1'>
+        <div>
+          <span>1</span>
+        </div>
+        
+        <div>
+          <span>2</span>
+        </div>
+      </div>
+      <div className='column number-2'>
+      <div>
+          <span>3</span>
+        </div>
+        <div>
+          <span>4</span>
+        </div>
+      </div>
     </div>
    );
 }
